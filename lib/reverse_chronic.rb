eddc1801382bfad88c2190a18f0bd358f2ef0470
Joe Sak <joe@joesak.com>
2012-02-08 09:07:55 -0600

    Pass initial spec for 'today' vs 'yesterday'

--- lib/reverse_chronic.rb
+++ lib/reverse_chronic.rb
@@ -1,5 +1,15 @@
 require "reverse_chronic/version"
 
 module ReverseChronic
-  # Your code goes here...
+  class << self
+    def humanize(time)
+      if time.to_date == Date.today
+        'today'
+      elsif Time.now.to_date - 1 == time.to_date
+        'yesterday'
+      else
+        time.strftime('%A, %b%e, %Y')
+      end
+    end
+  end
 end
